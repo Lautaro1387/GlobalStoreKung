23f0efa3ece04681f2919b33a64ca5d0723986cf
lautaro1387 <lautaro12387@gmail.com>
2024-04-28 23:47:26 -0300

    Various fix landing page

--- app/landing.tsx
+++ app/landing.tsx
@@ -4,14 +4,24 @@ export default function Landing() {
     return(
         <main className="flex flex-col items-right justify-between">
           <div className="bg-custom-bg py-4">
-            <div className="px-20">
-              <img
-                className="relative"
-                src="/logo_final.png"
-                alt="Logo"
-                width={200}
-                height={300}
-                />  
+            <div className='flex'>
+              <div className='px-5'>
+                <img
+                  className="relative"
+                  src="/logo_final.png"
+                  alt="Logo"
+                  width={210}
+                  height={300}
+                  />  
+              </div>
+              <div>
+                <button className=' justify-end'>
+                  <img src="/dark_mode.svg" alt="Dark Mode"
+                    height={200}
+                    width={40}
+                  />
+                </button>
+              </div>
             </div>
             <div className="flex flex-col lg:flex-row px-10 h-screen">
               <div className="w-full lg:w-1/2 py-20 sm:py-40 md:py-20 md:pr-20 lg:pr-20">
@@ -72,35 +82,61 @@ export default function Landing() {
                 <p className='px-10 pb-20'>Una vez tengas la pagina, podras vender cuantas veces quieras</p>
               </div>
           </div>
-          <div className="py-40">
-            <div className="flex px-20 h-screen">
-              <div className="w-1/2 py-20">
-                <h1 className="text-2x1 font-bold mb-4 text-6xl">Creá tu tienda online en minutos y vendé a todo el mundo!</h1>
-                <p className="text-2x1 text-lg pb-5">Podes crear promos, descuentos, aceptá pagos online y publicitá tu tienda. ¡Todo lo necesario para explotar tus ventas!</p>
-                <button className="hover:bg-slate-700 rounded-md text-2x1 bg-slate-950 text-gray-100 px-4 py-2 border-2 border-neutral-800">Empieza ahora</button>
-              </div>
-              <div className="flex px-10 w-1/2 justify-center items-center">
+          <div className="py-24">
+            <div className="flex flex-col lg:flex-row px-10 h-screen">
+              <div className="w-full lg:hidden">
                 <img
-                className="pb-10"
-                src="/landing_test.png"
-                alt="gif"
-                width={800}
-                height={100}
+                  className="max-w-full"
+                  src="/landing_page_p2.png"
+                  width={500}
+                  height={500}
+                  alt="gif"
                 />
               </div>
+              <div className="w-full lg:w-1/2 py-20 sm:py-40 md:py-20 md:pr-20 lg:pr-20">
+                <h1 className="text-5x1 text-center md:text-6xl font-bold sm:text-3xl mb-4">Te ayudamos todos los dias del año!</h1>
+                <p className="md:text-xl text-center text-2x1 text-lg pb-5">Nuestro equipo esta formado por personas reales, lo que significa que te ayudaremos y acompañaremos en lo que necesites para lograr vender tus productos!</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="justify-center items-center bg-slate-950 pb-10">
+              <h1 className="py-10 p-4 font-bold text-center text-2xl text-slate-100">ESTAMOS EN LINEA AHORA</h1>
+              <button className="hover:bg-slate-700 rounded-md mx-auto block text-2x1 bg-custom-bg text-gray-100 px-4 py-2 border-2 border-neutral-800">Chatea con nosotros</button>
+            </div>
+          </div>
+
+          <div className="py-24">
+            <div className="flex flex-col lg:flex-row px-10 h-screen">
+              <div className="w-full lg:hidden">
+                <img
+                  className="max-w-full"
+                  src="/landing_page_p2.png"
+                  width={500}
+                  height={500}
+                  alt="gif"
+                />
+              </div>
+            <div className="flex flex-col w-full md:w-1/3 justify-center items-center py-10">
+                <h1 className='font-bold py-2 text-2xl'> 1 - Elegis tu dominio .com</h1>
+                <p className='pb-20 px-6'>Tu eliges el logo, los productos y los colores, nosotros te lo colocamos!</p>
+                <h1 className='font-bold py-2 text-2xl'> 2 - Personalizas tu tienda</h1>
+                <p className='pb-20 px-6'>Solo nos indicas que productos necesitas y nosotros lo haremos!</p>
+                <h1 className='font-bold py-2 text-2xl'> 3 - Explota todas tus ventas</h1>
+                <p className='pb-20 px-6'>Una vez tengas la pagina, podras vender cuantas veces quieras</p>
+              </div>
             </div>
           </div>
           <div className="py-40">
-            <div className="flex justify-center items-center bg-slate-950 pb-10">
-              <h1 className="p-4 font-bold text-slate-100 w-56">ESTAMOS EN LINEA AHORA</h1>
-              <button className="hover:bg-slate-700 rounded-md text-2x1 bg-slate-950 text-gray-100 px-4 py-2 border-2 border-neutral-800">Chatea con nosotros</button>
+            <div className="flex justify-center items-center bg-slate-950 p-4">
+              <h1 className="font-bold text-slate-100 text-center px-10 py-10 text-2xl">UN UNICO PAGO A TAN SOLO USD300 CON TODO INCLUIDO</h1>
             </div>
           </div>
-          <div className="py-40">
-            <div className="flex justify-center items-center bg-slate-950 pb-10">
-              <h1 className="p-4 font-bold text-slate-100 w-56">PAGO UNICO A TAN SOLO $5000 CON TODO INCLUIDO</h1>
+          <div className="flex justify-center items-center font-bold text-2xl pb-20 px-10 text-center">
+              <h1>
+                Preguntas frecuentes
+              </h1>
           </div>
-      </div>
     </main>
     )
 }
