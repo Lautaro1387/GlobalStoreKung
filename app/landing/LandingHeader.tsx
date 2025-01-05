d5117ac01e9b37e4625c709fc99e96120f395806
Lautaro1387 <lautaro12387@gmail.com>
2025-01-05 12:38:29 -0300

    Add video in home

--- app/landing/LandingHeader.tsx
+++ app/landing/LandingHeader.tsx
@@ -3,34 +3,37 @@ import Image from 'next/image';
 import NavBar from "./LandingNavBar";
 import Link from "next/link";
 
-
 export default function LandingHeader() {
   return (
-    <div className="bg-teal-700">
-      <NavBar />
-      <div className="bg-teal-600 py-40 justify-between items-center text-start">
-        <div className="flex flex-col md:flex-row h-screen px-5 md:px-20 lg:px-30">
-          <div className="w-full md:w-1/2 md:pr-10 lg:pr-20">
-            <h1 className="md:text-left lg:text-left px-2 md:px-4 lg:px-10 text-5xl md:text-6xl lg:text-6xl font-bold mb-6">
-              Haz crecer tu negocio de la mejor manera
-            </h1>
-            <p className="text-lg md:text-xl lg:text-2xl md:text-left px-4 lg:px-10 pb-8">
-              Aplicamos estrategias y soluciones innovadoras para satisfacer las necesidades de tus clientes.
-            </p>
-            <Link className="hover:bg-slate-900 md:mx-8 mx-2 rounded-full text-xl bg-slate-950 text-gray-100 px-6 py-3 md:py-4 border-2 border-neutral-800 animate-bounce" href='/contacto'>
-            Contacta con nosotros
-            </Link>
-          </div>
-          <div className="w-full md:w-1/2 flex justify-center items-center mt-10 md:mt-0 lg:pb-56">
-            <Image
-              className="max-w-full p-5 lg:p-10"
-              src="/img/img_header.svg"
-              alt="gif"
-              width={480}
-              height={400}
-              />
-          </div>
-        </div>
+    <div className="relative h-screen w-full">
+      {/* Video de fondo */}
+      <NavBar></NavBar>
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/img/img_1.mp4" type="video/mp4" />
+      </video>
+
+      {/* Overlay para oscurecer el fondo */}
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
+
+      {/* Contenido */}
+      <div className="relative z-10 text-white flex flex-col justify-center items-center h-full px-6 text-center">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">
+          Haz crecer tu negocio de la mejor manera
+        </h1>
+        <p className="text-lg md:text-2xl mb-6">
+          Aplicamos estrategias y soluciones innovadoras para satisfacer las necesidades de tus clientes.
+        </p>
+        <a
+          href="/contacto"
+          className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-full text-lg transition duration-300"
+        >
+          Contacta con nosotros
+        </a>
       </div>
     </div>
   );
